113f5fcb6d30fa37d693ed122f125cf324732445
Prashant Sinha <prashant@ducic.ac.in>
2016-08-11 18:07:00 +0530

    + Exposure

--- cad/metrics/geometry.py
+++ cad/metrics/geometry.py
@@ -100,28 +100,6 @@ def find_convex_hull_rect(points):
   return vertex_pairs, length, breadth
 
 
-def compute_transformation_matrix(vertices, length, breadth):
-  predicate = lambda x: np.linalg.norm(x, ord=2)
-  source = [
-    [0, 0],
-    [0, length],
-    [breadth, length],
-    [breadth, 0]]
-  vertices = list(vertices)
-  source.sort(key=predicate)
-  vertices.sort(key=predicate)
-  src, dst = map(np.array, [source, vertices])
-
-  tf = skimage.transform.ProjectiveTransform()
-  tf.estimate(src, dst)
-  return tf
-
-
-def apply_transform(img, tf, length, breadth):
-  shape = (int(length), int(breadth))
-  return skimage.transform.warp(img, tf, output_shape=shape)
-
-
 def infer_lines(img, axis=0, **kwa):
   """Infer line span from broken segments.
 
--- cad/metrics/transform.py
+++ cad/metrics/transform.py
@@ -1,4 +1,24 @@
-import skimage.feature
+import numpy as np
 import skimage.transform
 
+def compute_transformation_matrix(vertices, length, breadth):
+  predicate = lambda x: np.linalg.norm(x, ord=2)
+  source = [
+    [0, 0],
+    [0, length],
+    [breadth, length],
+    [breadth, 0]]
+  vertices = list(vertices)
+  source.sort(key=predicate)
+  vertices.sort(key=predicate)
+  src, dst = map(np.array, [source, vertices])
+
+  tf = skimage.transform.ProjectiveTransform()
+  tf.estimate(src, dst)
+  return tf
+
+
+def apply_transform(img, tf, length, breadth):
+  shape = (int(length), int(breadth))
+  return skimage.transform.warp(img, tf, output_shape=shape)
 
