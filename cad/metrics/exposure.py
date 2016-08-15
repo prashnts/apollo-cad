50e9b8ac37e5f7eb35679d55531d8f26d081bc81
Prashant Sinha <prashant@ducic.ac.in>
2016-08-15 12:59:06 +0530

    + Adds transformation and further API improvements

--- cad/metrics/exposure.py
+++ cad/metrics/exposure.py
@@ -4,18 +4,15 @@ import skimage.filters
 import skimage.util
 
 
-BIN_COUNT = 10
-BLOCK_SIZE = 35
-KERNEL_SIZE = 50
-
+BIN_COUNT = 5
+KERNEL_SIZE = 200
+CLIP_LIMIT = 0.99
 
 def _to_grayscale(img):
   return skimage.color.rgb2gray(img)
 
-def equalize(img, kernel=KERNEL_SIZE, bins=BIN_COUNT, block=BLOCK_SIZE):
-  gimg = _to_grayscale(img)
-  adapted_histogram = (skimage.exposure
-      .equalize_adapthist(gimg, kernel_size=kernel, nbins=bins))
-  filtered_pxmap = (skimage.filters
-      .threshold_adaptive(adapted_histogram, block_size=block))
-  return skimage.util.img_as_float(filtered_pxmap)
+def equalize(img, kernel=KERNEL_SIZE, bins=BIN_COUNT, clip=CLIP_LIMIT):
+  eqimg = (skimage.exposure
+      .equalize_adapthist(img, kernel_size=kernel,
+          nbins=bins, clip_limit=CLIP_LIMIT))
+  return skimage.color.rgb2gray(eqimg)
